9fde87a530932566ecec391683d40f7441dae504
xAMATEURx <dimakrok@bk.ru>
2020-05-20 00:10:01 +0300

    ADD: first part of 7th lab

---
+++ lab_07/lab_07.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+extern "C" {
+	void testAsm(); // подключение в код на Си/Си++ функции
+	// на другом языке программирования,
+	// выполненной в соответствии с соглашениями
+	// о вызовах Си
+}
+
+int main() {
+	char testString[] = "Wow, such a program\0";
+	int result = 0;
+
+	__asm {
+		mov ECX, -1
+		mov AL, 0
+		lea DI, [testString]
+		repne scasb
+		mov EAX, -1
+		sub EAX, ECX
+		dec EAX
+		mov result, EAX
+	}
+	std::cout << result;
+	return 0;
+}
---
+++ lab_07/prog.asm
@@ -0,0 +1,9 @@
+.686
+.MODEL FLAT, C
+.STACK
+.CODE
+	testAsm PROC
+	mov eax, 7
+	ret
+testAsm ENDP
+END
