a9ba55bf77a34d1ea3ea160525de1b87af85101d
xAMATEURx <dimakrok@bk.ru>
2020-05-21 11:59:26 +0300

    ADD: starts

--- lab_08/lab_08/lab_08.cpp
+++ lab_08/lab_08/lab_08.cpp
@@ -1,6 +1,29 @@
 ﻿#include <iostream>
 
+using namespace std;
+
+float addFloat() {
+
+}
+
+float difFloat() {
+
+}
+
+float mulFloat() {
+
+}
+
+float divFloat() {
+
+}
+
 int main()
 {
-    // Калькулятор нормальный.
+	cout << "Set your epxr: ";
+	float firstNum = 0, secondNum = 0;
+	char oper = '+';
+
+	cin >> firstNum >> oper >> secondNum;
+	if (oper == '+')
 }
