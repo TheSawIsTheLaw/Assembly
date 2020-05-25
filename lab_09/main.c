19da72db0aa2bb46978e9681c55c1fe912c27c92
xAMATEURx <dimakrok@bk.ru>
2020-05-25 22:15:34 +0300

    ADD: УРА Я НАКОНЕЦ-ТО НА ПОЛНЫХ ПРАВАХ МОГУ ЛИТЬ МУСОР В РЕПОЗИТИРИЙ ВУХУ!!!!!!!

--- lab_09/main.c
+++ lab_09/main.c
@@ -3,18 +3,41 @@
 #define YES 1
 #define NO 0
 
+#define ITSAHUMAN 0
+
 #define SUCCESS 0
 
+/*
+ * Собственно, по заданному тесту нужно набрать 0 баллов.
+ * Прямо как на экзамене. Только тут не отчисляют.
+ */
+
 int firstQuestion() {
-    printf("\nFirst question is: Are you a machine?");
+    printf("\nFirst question is: Are you a machine? Answer: ");
     int ret = NO;
     scanf("%d", &ret);
 
     return ret;
 }
 
+int clarification() {
+    printf("\nAre you sure? Answer: ");
+    int notRet = NO;
+    scanf("%d", &notRet);
+
+    return notRet;
+}
+
 int secondQuestion() {
-    printf("");
+    printf("\nSecond question is: Are you a human? Answer: ");
+    int answ = NO;
+    scanf("%d", &answ);
+
+    if (answ == YES)
+        answ = clarification();
+    else
+        answ = YES;
+    return answ;
 }
 
 int main(void) {
@@ -24,6 +47,14 @@ int main(void) {
 
     mark += firstQuestion();
 
+    printf("%d", mark);
+    mark += secondQuestion();
+    printf("%d", mark);
+
+    if (mark == ITSAHUMAN)
+        printf("\nOh, hi, Mark!");
+    else
+        printf("Stupid cringeMachine! Go away!");
 
     return SUCCESS;
 }
